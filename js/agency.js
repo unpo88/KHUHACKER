b996858de986e8f5fa77fd984ef85f3e2ed5004a
GyuhoLee <gyuho965@naver.com>
2019-09-07 14:46:55 +0900

    [UPDATE] head, header, footer detach

--- js/agency.js
+++ js/agency.js
@@ -1,3 +1,9 @@
 $('.carousel').carousel({
 interval: 2000
 })
+
+$(function(){
+$(".navbar").load('/sys/header.html');
+$(".footer").load('/sys/footer.html');
+$(".head").load('/sys/head.html');
+});
